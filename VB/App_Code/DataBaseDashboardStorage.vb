72feb586a7c70067da6a39b71e9ea621d3f33ab6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/App_Code/DataBaseDashboardStorage.vb
@@ -0,0 +1,67 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Xml.Linq
+Imports DevExpress.DashboardWeb.Designer
+Imports System.Web
+Imports System.Web.SessionState
+Imports System.Data
+Imports System.IO
+Imports nWindTableAdapters
+Imports DevExpress.DashboardCommon
+
+Public Class DataBaseDashboardStorage
+    Implements IDashboardStorage
+
+    Private tableAdapter As New DashboardsTableAdapter()
+    Private dataSet As New nWind()
+
+
+    Public Sub New()
+        MyBase.New()
+        tableAdapter.Fill(dataSet.Dashboards)
+        'tableAdapter.Delete("dashboard1");
+
+    End Sub
+
+    Public Function CreateNewDashboard() As String Implements IDashboardStorage.CreateNewDashboard
+        Const IdPrefix As String = "dashboard"
+        Dim newDashboardId As String
+        Dim existentDashoards As IEnumerable(Of String) = (TryCast(Me, IDashboardStorage)).GetDashboardIDs()
+        Dim index As Integer = 1
+        Do
+            newDashboardId = String.Format("{0}{1}", IdPrefix, index)
+            index += 1
+        Loop While (TryCast(Me, IDashboardStorage)).GetDashboardIDs().Contains(newDashboardId)
+
+        Dim stream As New MemoryStream()
+        CType(New Dashboard(), Dashboard).SaveToXDocument().Save(stream)
+        stream.Position = 0
+        dataSet.Dashboards.Rows.Add(newDashboardId, stream.ToArray())
+
+        Return newDashboardId
+    End Function
+
+    Private Function IDashboardStorage_GetDashboard(ByVal id As String) As XDocument Implements IDashboardStorage.GetDashboard
+        Dim data() As Byte = CType(dataSet.Dashboards.Rows.Find(id)("DashboardData"), Byte())
+        Dim stream As New MemoryStream(data)
+        Return XDocument.Load(stream)
+    End Function
+
+    Private Function IDashboardStorage_GetDashboardIDs() As IEnumerable(Of String) Implements IDashboardStorage.GetDashboardIDs
+        Return dataSet.Dashboards.AsEnumerable().Select(Function(row) row.ID)
+    End Function
+
+    Private Sub IDashboardStorage_UpdateDashboard(ByVal id As String, ByVal document As XDocument) Implements IDashboardStorage.UpdateDashboard
+        Dim stream As New MemoryStream()
+        document.Save(stream)
+        stream.Position = 0
+        Dim row As DataRow = dataSet.Dashboards.Rows.Find(id)
+        If row IsNot Nothing Then
+            row("DashboardData") = stream.ToArray()
+        Else
+            dataSet.Dashboards.Rows.Add(id, stream.ToArray())
+        End If
+        tableAdapter.Update(dataSet.Dashboards)
+    End Sub
+End Class
---
+++ VB/Global.asax.vb
@@ -0,0 +1,51 @@
+﻿Imports System
+Imports System.Web.Hosting
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon.Native.DashboardRestfulService
+Imports DevExpress.DashboardWeb.Designer
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess.Sql
+
+Partial Public Class [Global]
+    Inherits System.Web.HttpApplication
+
+    Protected Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
+        Dim dataBaseDashboardStorage As New DataBaseDashboardStorage()
+        ASPxDashboardDesigner.Storage.SetDashboardStorage(dataBaseDashboardStorage)
+
+'        #Region "XML Data Source"
+        Dim accessParams As New Access97ConnectionParameters()
+        accessParams.FileName = Server.MapPath("~/App_Data/nwind.mdb")
+        Dim sqlDataSource As New DashboardSqlDataSource("SQL Data Source", accessParams)
+        Dim customerReportsQuery As New TableQuery("CustomerReports")
+        customerReportsQuery.AddTable("CustomerReports").SelectColumns("CompanyName", "ProductName", "OrderDate", "ProductAmount")
+        sqlDataSource.Queries.Add(customerReportsQuery)
+'        #End Region
+
+        Dim dataSourceStorage As New DataSourceInMemoryStorage()
+        dataSourceStorage.RegisterDataSource("sqlDataSource1", sqlDataSource)
+        ASPxDashboardDesigner.Storage.SetDataSourceStorage(dataSourceStorage)
+        AddHandler ASPxDashboardDesigner.Storage.ConfigureDataConnection, AddressOf Storage_ConfigureDataConnection
+    End Sub
+
+    Protected Sub Storage_ConfigureDataConnection(ByVal sender As Object, ByVal e As ConfigureServiceDataConnectionEventArgs)
+        If e.DataSourceName = "SQL Data Source" Then
+            Dim accessParams As Access97ConnectionParameters = TryCast(e.ConnectionParameters, Access97ConnectionParameters)
+            accessParams.FileName = HostingEnvironment.MapPath("~/App_Data/nwind.mdb")
+        End If
+    End Sub
+
+
+    Protected Sub Session_Start(ByVal sender As Object, ByVal e As EventArgs)
+    End Sub
+    Protected Sub Application_BeginRequest(ByVal sender As Object, ByVal e As EventArgs)
+    End Sub
+    Protected Sub Application_AuthenticateRequest(ByVal sender As Object, ByVal e As EventArgs)
+    End Sub
+    Protected Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
+    End Sub
+    Protected Sub Session_End(ByVal sender As Object, ByVal e As EventArgs)
+    End Sub
+    Protected Sub Application_End(ByVal sender As Object, ByVal e As EventArgs)
+    End Sub
+End Class
